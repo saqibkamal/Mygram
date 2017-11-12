9bae2750fa1d4c23fbf142c9ccb7b78a258f8c22
Saqib kamal <saqib.kamal01@gmail.com>
2017-11-13 02:01:44 +0530

    Profile Page Updated

---
+++ app/src/main/java/kamal/saqib/mygram/Mygram.java
@@ -0,0 +1,13 @@
+package kamal.saqib.mygram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mygram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mygram);
+    }
+}
---
+++ app/src/main/java/kamal/saqib/mygram/splash_activity.java
@@ -0,0 +1,13 @@
+package kamal.saqib.mygram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+    }
+}
